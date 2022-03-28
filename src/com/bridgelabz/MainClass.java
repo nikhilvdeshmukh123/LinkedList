8afb3c2e615483f9f6688500ff4c6fcc5c154e3c
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-03-28 06:04:38 +0530

    Added:Linked List Appending

--- src/com/bridgelabz/MainClass.java
+++ src/com/bridgelabz/MainClass.java
@@ -4,9 +4,9 @@ public class MainClass {
 
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
-        linkedList.addFirst(70);
-        linkedList.addFirst(30);
-        linkedList.addFirst(56);
+        linkedList.addLast(56);
+        linkedList.addLast(30);
+        linkedList.addLast(70);
         linkedList.show();
     }
 }
